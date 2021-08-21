265c32c1c92014e5d0130af7fc2e370563c3b752
Aralda Pacurar <aralda.pacurar@outlook.com>
2021-08-21 17:53:02 +0300

    Added latest users that forked the gist

--- gists/services.py
+++ gists/services.py
@@ -4,6 +4,7 @@ import requests
 class GithubServices:
     def __init__(self):
         self.user_gists_base_path = "https://api.github.com/users"
+        self.gist_forks_base_path = "https://api.github.com/gists"
 
     def make_request(self, url):
         response = requests.get(url)
@@ -12,6 +13,10 @@ class GithubServices:
     def make_user_gist_request(self, user):
         request_url = f"{self.user_gists_base_path}/{user}/gists"
         return self.make_request(request_url)
+
+    def make_gist_forks_request(self, gist_id):
+        requests_url = f"{self.gist_forks_base_path}/{gist_id}/forks"
+        return self.make_request(requests_url)
 
 
 githubServices = GithubServices()
@@ -32,3 +37,14 @@ def get_gist_information(gist_files):
     tags = list(tags)
     tags.sort(key=len)
     return files, tags
+
+
+def get_gist_forks_information(gist_forks):
+    fork_information = []
+    for crt_fork in gist_forks:
+        crt_fork_information = {
+            "fork_owner": crt_fork.get("owner", {}).get("login", ""),
+            "fork_owner_avatar": crt_fork.get("owner", {}).get("avatar_url", "")
+        }
+        fork_information.append(crt_fork_information)
+    return fork_information
--- gists/views.py
+++ gists/views.py
@@ -1,13 +1,17 @@
 import requests
 from django.shortcuts import render
 from django.contrib import messages
-from .services import githubServices, get_gist_information
+from .services import (
+    githubServices,
+    get_gist_information,
+    get_gist_forks_information
+)
 
 
 def homepage(request):
     context = {}
-    while True:
-        if request.method == "POST":
+    if request.method == "POST":
+        while True:
             github_user = request.POST.get("github_user", "")
             user_gists = githubServices.make_user_gist_request(github_user)
             if not user_gists.ok:
@@ -26,9 +30,27 @@ def homepage(request):
                 crt_gist = {
                     "files": files,
                     "file_tags": tags,
-                    "description": user_gist.get("description", "")
+                    "description": user_gist.get("description", ""),
+                    "forks": []
                 }
+
+                # Get the latest forks for the current gist
+                gist_id = user_gist.get("id", "")
+                gist_forks = githubServices.make_gist_forks_request(gist_id)
+                if not gist_forks.ok:
+                    gists.append(crt_gist)
+                    continue
+                try:
+                    gist_forks_json = gist_forks.json()
+                    gist_forks_json = gist_forks_json[-3:]  # Get the 3 latest forks
+                except requests.exceptions.JSONDecodeError:
+                    messages.error(request, f"Failed retrieving forks for gist {gist_id} of user {github_user}",
+                                   extra_tags="danger")
+                    gists.append(crt_gist)
+                    break
+                crt_gist["forks"] = get_gist_forks_information(gist_forks_json)
                 gists.append(crt_gist)
+
             gists_count = len(user_gists_jsons)
             context = {
                 "gist_owner": user_gists_jsons[0].get("owner", {}).get("login", "") if gists_count else "",
