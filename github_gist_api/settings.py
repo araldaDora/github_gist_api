63f548f84c2224cea0871deff89601f9ed875a9b
Aralda Pacurar <aralda.pacurar@outlook.com>
2021-08-21 10:51:45 +0300

    Created gists application

--- github_gist_api/settings.py
+++ github_gist_api/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'gists.apps.GistsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
