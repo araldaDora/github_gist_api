7d09e4c5847b4bf019b782848fb78e85a6c193f0
Aralda Pacurar <aralda.pacurar@outlook.com>
2021-08-21 23:03:35 +0300

    Made fix so that all the gists are retrieved not just the first page

--- gists/services.py
+++ gists/services.py
@@ -10,8 +10,8 @@ class GithubServices:
         response = requests.get(url)
         return response
 
-    def make_user_gist_request(self, user):
-        request_url = f"{self.user_gists_base_path}/{user}/gists"
+    def make_user_gist_request(self, user, page=1):
+        request_url = f"{self.user_gists_base_path}/{user}/gists?page={page}"
         return self.make_request(request_url)
 
     def make_gist_forks_request(self, gist_id, page=1):
@@ -22,16 +22,37 @@ class GithubServices:
 githubServices = GithubServices()
 
 
-def get_gists_for_user(github_user):
+def get_gists_for_given_page(github_user, page):
     status = -1
     user_gists_jsons = []
-    user_gists = githubServices.make_user_gist_request(github_user)
+    user_gists = githubServices.make_user_gist_request(github_user, page)
     if user_gists.ok:
         try:
             user_gists_jsons = user_gists.json()
             status = 0
         except requests.exceptions.JSONDecodeError:
             print("Failed decoding gist json")
+    return status, user_gists, user_gists_jsons
+
+
+def get_gists_for_user(github_user):
+    status = -1
+    user_gists_jsons = []
+    status, user_gists, user_gists_jsons = get_gists_for_given_page(github_user, 1)
+    previous_page = 0
+    crt_page = 1
+    while "next" in user_gists.links and previous_page != crt_page:
+        link = user_gists.links
+        next_page_url = link.get("next", {}).get("url", "")
+        if next_page_url == "":
+            break
+        page_nb_index = next_page_url.rfind("=") + 1
+        next_page = int(next_page_url[page_nb_index:])
+        status, _, next_user_gists_jsons = get_gists_for_given_page(github_user, next_page)
+        user_gists_jsons.extend(next_user_gists_jsons)
+
+        previous_page = crt_page
+        crt_page = next_page
     return status, user_gists_jsons
 
 
@@ -57,13 +78,13 @@ def get_latest_forks_for_gist(gist_id, fork_count):
         link = gist_forks.links
         if link:
             last_page_url = link.get("last", {}).get("url", "")
-            page_nb_index = last_page_url.rfind("=") + 1
-            last_page = int(last_page_url[page_nb_index:])
-
-            # Get the newest forks --> from the last page
-            status, _, gist_forks_json = get_forks_for_given_page(gist_id, last_page)
-            if status == 0 and len(gist_forks_json) < 3 and last_page != 1:
-                status, _, previous_gist_forks_json = get_forks_for_given_page(gist_id, last_page-1)
+            if last_page_url != "":
+                page_nb_index = last_page_url.rfind("=") + 1
+                last_page = int(last_page_url[page_nb_index:])
+                # Get the newest forks --> from the last page
+                status, _, gist_forks_json = get_forks_for_given_page(gist_id, last_page)
+                if status == 0 and len(gist_forks_json) < 3 and last_page != 1:
+                    status, _, previous_gist_forks_json = get_forks_for_given_page(gist_id, last_page-1)
     if status == 0:
         previous_gist_forks_json.extend(gist_forks_json)
         gist_forks_json = previous_gist_forks_json[-3:]
