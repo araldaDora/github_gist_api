3ada002d0f540b385ece0abbc6a2d227861b0d3b
Aralda Pacurar <aralda.pacurar@outlook.com>
2021-08-21 11:55:23 +0300

    Created Gist, Owner and GistFile models

--- gists/admin.py
+++ gists/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Gist, Owner, GistFile
 
-# Register your models here.
+admin.site.register(Gist)
+admin.site.register(Owner)
+admin.site.register(GistFile)
