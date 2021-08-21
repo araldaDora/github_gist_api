770e1d4c5689146fcedfb5eff0b3760bdab4bbc4
Aralda Pacurar <aralda.pacurar@outlook.com>
2021-08-21 15:35:53 +0300

    Get gists for a given user. Initial template format to display the data

--- gists/services.py
+++ gists/services.py
@@ -1,6 +1,17 @@
 import requests
 
 
-def make_request(url):
-    response = requests.get(url)
-    return response
+class GithubServices:
+    def __init__(self):
+        self.user_gists_base_path = "https://api.github.com/users"
+
+    def make_request(self, url):
+        response = requests.get(url)
+        return response
+
+    def make_user_gist_request(self, user):
+        request_url = f"{self.user_gists_base_path}/{user}/gists"
+        return self.make_request(request_url)
+
+
+githubServices = GithubServices()
--- gists/views.py
+++ gists/views.py
@@ -1,5 +1,34 @@
+import requests
 from django.shortcuts import render
+from django.contrib import messages
+from .services import githubServices
 
 
 def homepage(request):
-    return render(request, "gists/home.html")
+    context = {}
+    while True:
+        if request.method == "POST":
+            github_user = request.POST.get("github_user", "")
+            user_gists = githubServices.make_user_gist_request(github_user)
+            if not user_gists.ok:
+                break
+
+            try:
+                user_gists_jsons = user_gists.json()
+            except requests.exceptions.JSONDecodeError:
+                messages.error(request, f"Failed retrieving gists for user {github_user}", extra_tags="danger")
+                break
+
+            gists = []
+            for user_gist in user_gists_jsons:
+                crt_gist = {
+                    "files": [file_data for _, file_data in user_gist.get("files", {}).items()],
+                    "owner_login": user_gist.get("owner", {}).get("login", ""),
+                    "owner_avatar": user_gist.get("owner", {}).get("avatar_url", "")
+                }
+                gists.append(crt_gist)
+            context = {
+                "gists": gists
+            }
+            break
+    return render(request, "gists/home.html", context=context)
