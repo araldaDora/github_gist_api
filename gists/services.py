73b78d0014da17f0a9bd278b41029ee421ff6b96
Aralda Pacurar <aralda.pacurar@outlook.com>
2021-08-21 18:24:42 +0300

    Refactored code in homepage view

--- gists/services.py
+++ gists/services.py
@@ -22,9 +22,37 @@ class GithubServices:
 githubServices = GithubServices()
 
 
-def get_gist_information(gist_files):
+def get_gists_for_user(github_user):
+    status = -1
+    user_gists_jsons = []
+    user_gists = githubServices.make_user_gist_request(github_user)
+    if user_gists.ok:
+        try:
+            user_gists_jsons = user_gists.json()
+            status = 0
+        except requests.exceptions.JSONDecodeError:
+            print("Failed decoding gist json")
+    return status, user_gists_jsons
+
+
+def get_latest_forks_for_gist(gist_id, fork_count):
+    status = -1
+    gist_forks_json = []
+    gist_forks = githubServices.make_gist_forks_request(gist_id)
+    if gist_forks.ok:
+        try:
+            gist_forks_json = gist_forks.json()
+            gist_forks_json = gist_forks_json[-fork_count:]  # Get the latest forks
+            status = 0
+        except requests.exceptions.JSONDecodeError:
+            print("Failed decoding fork json")
+    return status, gist_forks_json
+
+
+def get_gist_information(gist):
     tags = set()
     files = []
+    gist_files = [file_json for _, file_json in gist.get("files", {}).items()]
     for f in gist_files:
         crt_file = {
             "filename": f.get("filename", ""),
@@ -33,7 +61,6 @@ def get_gist_information(gist_files):
         file_type = f.get("type", "")
         tags.add(str(file_type))
         files.append(crt_file)
-
     tags = list(tags)
     tags.sort(key=len)
     return files, tags
--- gists/views.py
+++ gists/views.py
@@ -1,8 +1,8 @@
-import requests
 from django.shortcuts import render
 from django.contrib import messages
 from .services import (
-    githubServices,
+    get_gists_for_user,
+    get_latest_forks_for_gist,
     get_gist_information,
     get_gist_forks_information
 )
@@ -11,51 +11,35 @@ from .services import (
 def homepage(request):
     context = {}
     if request.method == "POST":
-        while True:
-            github_user = request.POST.get("github_user", "")
-            user_gists = githubServices.make_user_gist_request(github_user)
-            if not user_gists.ok:
-                break
+        github_user = request.POST.get("github_user", "")
+        status, user_gists_jsons = get_gists_for_user(github_user)
+        if status != 0:
+            user_gists_jsons = []
+            messages.error(request, f"Failed retrieving gists for {github_user}", extra_tags="danger")
+        gists = []
+        for user_gist in user_gists_jsons:
+            files, tags = get_gist_information(user_gist)
+            crt_gist = {
+                "files": files,
+                "file_tags": tags,
+                "description": user_gist.get("description", ""),
+                "forks": []
+            }
+            # Get the latest forks for the current gist
+            gist_id = user_gist.get("id", "")
+            status, gist_forks_json = get_latest_forks_for_gist(gist_id, 3)
+            if status != 0:
+                gist_forks_json = []
+                messages.error(request, f"Failed retrieving forks for gist {gist_id} of user {github_user}",
+                               extra_tags="danger")
+            crt_gist["forks"] = get_gist_forks_information(gist_forks_json)
+            gists.append(crt_gist)
 
-            try:
-                user_gists_jsons = user_gists.json()
-            except requests.exceptions.JSONDecodeError:
-                messages.error(request, f"Failed retrieving gists for user {github_user}", extra_tags="danger")
-                break
-
-            gists = []
-            for user_gist in user_gists_jsons:
-                files = [file_json_info for _, file_json_info in user_gist.get("files", {}).items()]
-                files, tags = get_gist_information(files)
-                crt_gist = {
-                    "files": files,
-                    "file_tags": tags,
-                    "description": user_gist.get("description", ""),
-                    "forks": []
-                }
-
-                # Get the latest forks for the current gist
-                gist_id = user_gist.get("id", "")
-                gist_forks = githubServices.make_gist_forks_request(gist_id)
-                if not gist_forks.ok:
-                    gists.append(crt_gist)
-                    continue
-                try:
-                    gist_forks_json = gist_forks.json()
-                    gist_forks_json = gist_forks_json[-3:]  # Get the 3 latest forks
-                except requests.exceptions.JSONDecodeError:
-                    messages.error(request, f"Failed retrieving forks for gist {gist_id} of user {github_user}",
-                                   extra_tags="danger")
-                    gists.append(crt_gist)
-                    break
-                crt_gist["forks"] = get_gist_forks_information(gist_forks_json)
-                gists.append(crt_gist)
-
-            gists_count = len(user_gists_jsons)
+        gists_count = len(user_gists_jsons)
+        if gists_count:
             context = {
                 "gist_owner": user_gists_jsons[0].get("owner", {}).get("login", "") if gists_count else "",
                 "gist_owner_avatar": user_gists_jsons[0].get("owner", {}).get("avatar_url", ""),
                 "gists": gists
             }
-            break
     return render(request, "gists/home.html", context=context)
